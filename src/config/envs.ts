430a7c5fe16e32589bd4b8f2618e4d2edbe75838
DelioCoder <davideveriwhere@gmail.com>
2025-07-30 13:16:56 -0500

    Unnecessary environments commented

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,16 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
-    PRODUCTS_MICROSERVICE_HOST: string;
-    PRODUCTS_MICROSERVICE_PORT: number;
+    // PRODUCTS_MICROSERVICE_HOST: string;
+    // PRODUCTS_MICROSERVICE_PORT: number;
 
     NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    // PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    // PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
 
     NATS_SERVERS: joi.array().items( joi.string() ).required(),
 })
@@ -32,7 +32,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
-    productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-    productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
+    // productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+    // productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
     NATS_SERVERS: envVars.NATS_SERVERS,
 }
